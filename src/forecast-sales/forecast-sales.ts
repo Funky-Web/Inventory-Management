9a401f9d86bc412c62f31ae9f779569eed50728d
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-06-18 17:11:52 +0000

    Update

--- src/forecast-sales/forecast-sales.ts
+++ src/forecast-sales/forecast-sales.ts
@@ -1,20 +1,30 @@
 import {Component, inject} from '@angular/core';
+import {Dialog, DialogModule} from '@angular/cdk/dialog';
 import {Product} from '../core/product';
 import {ProductsService} from '../core/dao/products-service';
+import {AddNewProductDialog} from '../shared/add-new-product-dialog/add-new-product-dialog';
 
 @Component({
   selector: 'app-forecast-sales',
-  imports: [],
+  standalone: true,
+  imports: [DialogModule],
   templateUrl: './forecast-sales.html',
   styleUrl: './forecast-sales.css'
 })
 export class ForecastSales {
-
   productsService: ProductsService = inject(ProductsService);
-
   allProducts: Product[] = [];
+  selectedProduct: Product | null = null;
 
-  constructor() {
+  constructor(public dialog: Dialog) {
     this.allProducts = this.productsService.getAllProducts();
+  }
+
+  openDialog(product: Product): void {
+    this.selectedProduct = product;
+
+    this.dialog.open(AddNewProductDialog, {
+      data: { product }
+    });
   }
 }
